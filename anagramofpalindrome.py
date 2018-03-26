76d8118222bc834a295ca9145106b59ef071160c
Mansi Agarwal <mansi.agarwal1410@gmail.com>
2018-03-25 22:45:08 -0700

    check if a word is anagram of palindrom of a word

---
+++ anagramofpalindrome.py
@@ -0,0 +1,57 @@
+"""Is the word an anagram of a palindrome?
+
+A palindrome is a word that reads the same forward and backwards
+(eg, "racecar", "tacocat"). An anagram is a rescrambling of a word
+(eg for "racecar", you could rescramble this as "arceace").
+
+Determine if the given word is a re-scrambling of a palindrome.
+The word will only contain lowercase letters, a-z.
+
+Examples::
+
+    >>> is_anagram_of_palindrome("a")
+    True
+
+    >>> is_anagram_of_palindrome("ab")
+    False
+
+    >>> is_anagram_of_palindrome("aab")
+    True
+
+    >>> is_anagram_of_palindrome("arceace")
+    True
+
+    >>> is_anagram_of_palindrome("arceaceb")
+    False
+
+"""
+
+
+def is_anagram_of_palindrome(word):
+    """Is the word an anagram of a palindrome?"""
+
+    # In this question ,we will check if the word is a palindrom using brute force way
+
+    dict_letters = {}
+    odds_seen = 0
+
+    for letter in word:
+        dict_letters[letter] = dict_letters.get(letter, 0) + 1
+
+    for letter in dict_letters:
+        if dict_letters[letter] % 2 == 1:
+            odds_seen += 1
+
+        if odds_seen > 1:
+            return False
+
+    return True
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TEST PASSED. AWESOMESAUCE!\n"
