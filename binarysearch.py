e1c6714b37971f9189f25afa930a263711a7d951
Mansi Agarwal <mansi.agarwal1410@gmail.com>
2018-03-27 12:55:13 -0700

    binary search

---
+++ binarysearch.py
@@ -0,0 +1,52 @@
+"""Using a binary search, find val in a range of 1-100. Return # of guesses.
+
+Construct a list of 1-100 (inclusive). Write a binary search that searches
+for val in that list (val will always be a number between 1 and 100).
+
+Return the number of searches it took to find val. For a proper binary search
+of 1-100, this should never be more than 7.
+
+    >>> binary_search(50)
+    1
+
+    >>> binary_search(25)
+    2
+
+    >>> binary_search(75)
+    2
+
+    >>> binary_search(31) <= 7
+    True
+
+    >>> max([binary_search(i) for i in range(1, 101)])
+    7
+"""
+
+def binary_search(val):
+    """Using binary search, find val in range 1-100. Return # of guesses."""
+
+    assert 0 < val < 101, "Val must be between 1-100"
+
+    num_guesses = 0
+
+    my_list = range(0,101)
+
+    while my_list:
+        num_guesses += 1
+        mid = int(len(my_list)/2)
+        if val == my_list[mid]:
+            break
+        elif val < my_list[mid]:
+            my_list = my_list[:mid]
+        elif val > my_list[mid]:
+            my_list = my_list[mid:]
+
+
+
+    return num_guesses
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. YOU'RE TERRIFIC AT THIS!\n"
