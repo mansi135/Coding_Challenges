9c096ddddb02d3db397248478f774296cff3249e
Mansi Agarwal <mansi.agarwal1410@gmail.com>
2018-03-27 12:55:41 -0700

    Given a chessboard with one K and one Q, see if the Q can attack the K

---
+++ check.py
@@ -0,0 +1,112 @@
+"""Given a chessboard with one K and one Q, see if the K can attack the Q.
+
+This function is given coordinates for the king and queen on a chessboard.
+These coordinates are given as a letter A-H for the columns and 1-8 for the
+row (see below for example):
+
+Queens can move in any direction: horizontally, vertically, or diagonally,
+as far as possible.
+
+This function returns True if the king is in the line of attack of the queen.
+
+For example, these boards show the king under attack:
+
+8    . . . . . . . .      . . . . . . . .      . . . . . . . .    8
+7    . . . . . . . .      . . . . . . . .      . K . . . . . .    7
+6    . . . K . . . Q      . . . . K . . .      . . . . . . . .    6
+5    . . . . . . . .      . . . . . . . .      . . . Q . . . .    5
+4    . . . . . . . .      . . . . Q . . .      . . . . . . . .    4
+3    . . . . . . . .      . . . . . . . .      . . . . . . . .    3
+2    . . . . . . . .      . . . . . . . .      . . . . . . . .    2
+1    . . . . . . . .      . . . . . . . .      . . . . . . . .    1
+     A B C D E F G H      A B C D E F G H      A B C D E F G H
+
+     K=D6, Q=H6           K=E6, Q=E4           K=B7, Q=D5
+
+>>> check("D6", "H6")
+True
+
+>>> check("E6", "E4")
+True
+
+>>> check("B7", "D5")
+True
+
+>>> check("A1", "H8")
+True
+
+>>> check("A8", "H1")
+True
+
+>>> check("D6", "H7")
+False
+
+>>> check("E6", "F4")
+False
+"""
+
+
+def check(king, queen):
+    """Given a chessboard with one K and one Q, see if the K can attack the Q.
+
+    This function is given coordinates for the king and queen on a chessboard.
+    These coordinates are given as a letter A-H for the columns and 1-8 for the
+    row, like "D6" and "B7":
+    """
+    if king[0] == queen[0] or king[1] == queen[1]:     # same col or same row
+        return True
+
+    row_map = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8}
+
+    ki = int(row_map[king[0]])
+    kj = int(king[1])
+
+    qi = int(row_map[queen[0]])
+    qj = int(queen[1])
+
+    return abs(ki-qi) == abs(kj-qj)
+
+    #return False
+
+    # i = int(qi)
+    # j = int(qj)
+    # while i < 9 and j < 9 :
+    #     if i == ki and j == kj:
+    #         return True
+    #     else:
+    #         i += 1
+    #         j += 1
+
+    # i = int(qi)
+    # j = int(qj)
+    # while i < 9 and j >= 0 :
+    #     if i == ki and j == kj:
+    #         return True
+    #     else:
+    #         i += 1
+    #         j -= 1
+
+    # i = int(qi)
+    # j = int(qj)
+    # while i >= 0 and j < 9 :
+    #     if i == ki and j == kj:
+    #         return True
+    #     else:
+    #         i -= 1
+    #         j += 1
+
+    # i = int(qi)
+    # j = int(qj)
+    # while i >= 0 and j >= 0 :
+    #     if i == ki and j == kj:
+    #         return True
+    #     else:
+    #         i -= 1
+    #         j -= 1
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. EXCELLENT GAME!\n"
+
